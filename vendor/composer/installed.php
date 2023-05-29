9a73fd0e0b0a8ff4b9c7104536ea582d08becb20
ibsve <m.simonetti@ibs.ve.it>
2023-05-29 08:37:27 +0200

    esclusa commit cartella vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'infocamere/egov',
+        'pretty_version' => '1.3.7',
+        'version' => '1.3.7.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'infocamere/egov' => array(
+            'pretty_version' => '1.3.7',
+            'version' => '1.3.7.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mashape/unirest-php' => array(
+            'pretty_version' => 'v3.0.4',
+            'version' => '3.0.4.0',
+            'reference' => '842c0f242dfaaf85f16b72e217bf7f7c19ab12cb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mashape/unirest-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
